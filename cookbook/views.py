5b670b81c0a0345cca6364092f7281064e2164a5
karmauh <mikolaj.bozko01@gmail.com>
2023-03-05 20:43:24 +0100

    delete view class

--- cookbook/views.py
+++ cookbook/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import ListView, UpdateView
+from django.views.generic import ListView, UpdateView, DeleteView
 from .models import CookBook
 from .forms import EditRecipeForm
 from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 class HomeView(ListView):
     model = CookBook
     template_name = 'home.html'
+   
     
 class RecipesView(ListView):
     model = CookBook
     template_name = 'all_recipes.html'
     context_object_name = 'cook_items'
     ordering = ['-id']
+   
     
 class EditRecipeView(UpdateView):
     model = CookBook
@@ -24,4 +27,10 @@ class EditRecipeView(UpdateView):
         form.instance.post_id = self.kwargs['pk']
         return super().form_valid(form)
     
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('recipes')
+   
+    
+class DeleteRecipeView(DeleteView):
+    model = CookBook
+    template_name = 'delete_recipe.html'
+    success_url = reverse_lazy('recipes')
