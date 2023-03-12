3f9cc832d2c59dcaa8631a3727ed82c12f8df776
karmauh <mikolaj.bozko01@gmail.com>
2023-03-12 16:46:56 +0100

    form for adding recipe

--- cookbook/forms.py
+++ cookbook/forms.py
@@ -12,3 +12,16 @@ class EditRecipeForm(forms.ModelForm):
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 20}),
             'snippet': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
         }
+        
+
+class AddRecipeForm(forms.ModelForm):
+    class Meta:
+        model = CookBook
+        fields = ('author', 'title', 'description',  'snippet')
+        
+        widgets = {
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 20}),
+            'snippet': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+        }
